3734f60b61c1ac7818552d62eaa4571364fa933c
femosc2 <felixmorau@gmail.com>
2021-10-01 13:50:55 +0200

    some more stuff

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import { useGetPokemonByNameQuery } from '../services/api';
 
 const Pokemon = () => {
-  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  const [pokemon, setPokemon] = useState("bulbasaur");
+  const { data, error, isLoading } = useGetPokemonByNameQuery(pokemon);
+  const pokemonData = useSelector<any, any>(
+    (state) => state.pokemonApi.queries
+  );
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    console.log(pokemonData[`getPokemonByName("${pokemon}")`]);
+  }, [pokemonData, pokemon]);
   return (
     <>
       {isLoading && <h2>Det laddar!</h2>}
@@ -17,6 +22,7 @@ const Pokemon = () => {
           <h3>Weight: {data.weight}</h3>
         </section>
       )}
+      <input onChange={(e) => setPokemon(e.target.value)} />
     </>
   );
 };
